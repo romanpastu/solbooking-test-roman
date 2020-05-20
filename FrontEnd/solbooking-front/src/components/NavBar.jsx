0461ca9efb4ca8ceb805391aa4b136adf0c74ffc
romanpastu <roman.pastushenko.1997@gmail.com>
2020-05-20 12:55:22 +0200

    redux implementation to store the userName

--- FrontEnd/solbooking-front/src/components/NavBar.jsx
+++ FrontEnd/solbooking-front/src/components/NavBar.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPowerOff} from '@fortawesome/free-solid-svg-icons'
+import { connect } from "react-redux"
 import './NavBar.css'
-export default class Navbar extends Component {
+
+const mapStateToProps = state => {
+    return {
+        userName: state.userName
+    }
+}
+
+class Navbar extends Component {
     constructor(props){
         super(props);
         this.state = {
@@ -18,9 +26,11 @@ export default class Navbar extends Component {
     render(){
         return(
             <div className="appNavbar">
-                <div className="navEl nav1">Bienvenid@ XXXX</div>
+                <div className="navEl nav1">Bienvenid@ {this.props.userName}</div>
                 <div className="navEl nav3"><FontAwesomeIcon icon={faPowerOff} className="navIcon" onClick={this.logout}/></div>
             </div>
         )
     }
 }
+
+export default connect(mapStateToProps)(Navbar)
---
+++ FrontEnd/solbooking-front/src/redux/reducers/reduxReducers.js
@@ -0,0 +1,17 @@
+import {SET_USER_NAME} from "../constants/action-types"
+
+const initialState ={
+    userName: "Hola"
+}
+
+function rootReducer(state = initialState, action){
+    switch(action.type){
+        case SET_USER_NAME : {
+            return { ...state, userName: action.payload }
+        }
+        default:
+            return state
+    }
+}
+
+export default rootReducer;
